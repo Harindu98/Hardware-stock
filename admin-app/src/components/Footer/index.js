a1a358a7dfc2071de138d5150edb8c09a25aa400
Harindu98 <hiranthadesilva11@gmail.com>
2021-04-19 11:46:56 +0530

    first commit

---
+++ admin-app/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+/**
+* @author
+* @function Footer
+**/
+
+const Footer = (props) => {
+  return(
+    <div class="footer">All reververs @2021</div>
+   )
+
+ }
+
+export default Footer;
